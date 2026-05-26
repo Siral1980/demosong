b3a3bf8927f826287d6dd5625d9e00a935a34a20
In Constant Pain <pmarra@studenti.apuliadigital.it>
2026-05-26 10:54:20 +0200

    Wut

--- src/main/java/it/its/demo/controllers/SongController.java
+++ src/main/java/it/its/demo/controllers/SongController.java
@@ -1,14 +1,21 @@
+//Uuuuuuh
 package it.its.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.its.demo.models.Song;
 import it.its.demo.services.SongService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/songs") // Endpoint base al plurale, come da buona pratica REST
